4eff335a8439460a7e15851aad1c88a3fb4c6901
FabianusMaximus <leppfabian@gmail.com>
2022-12-26 23:29:15 +0100

    part 1 gelöst

--- Day8/Part_1.py
+++ Day8/Part_1.py
@@ -2,33 +2,33 @@ from Day8.Utility import *
 
 
 def check_left(tree_arr: list, current_x: int, current_y: int) -> bool:
-    left_size = current_x
+    left_size = current_y
     for i in range(left_size):
-        if tree_arr[current_y][i] > tree_arr[current_x][current_y]:
+        if tree_arr[current_x][i] >= tree_arr[current_x][current_y]:
             return False
     return True
 
 
 def check_right(tree_arr: list, current_x: int, current_y: int) -> bool:
-    right_size = len(tree_arr[current_y])
-    for i in range(current_x, right_size):
-        if tree_arr[current_y][i] > tree_arr[current_x][current_y]:
+    right_size = len(tree_arr[current_x])
+    for i in range(current_y + 1, right_size):
+        if tree_arr[current_x][i] >= tree_arr[current_x][current_y]:
             return False
     return True
 
 
 def check_down(tree_arr: list, current_x: int, current_y: int) -> bool:
     down_size = len(tree_arr)
-    for i in range(current_y, down_size):
-        if tree_arr[current_x][i] > tree_arr[current_x][current_y]:
+    for i in range(current_x + 1, down_size):
+        if tree_arr[i][current_y] >= tree_arr[current_x][current_y]:
             return False
     return True
 
 
 def check_up(tree_arr: list, current_x: int, current_y: int) -> bool:
-    up_size = current_y
+    up_size = current_x
     for i in range(up_size):
-        if tree_arr[current_x][i] > tree_arr[current_x][current_y]:
+        if tree_arr[i][current_y] >= tree_arr[current_x][current_y]:
             return False
     return True
 
@@ -46,9 +46,9 @@ size_tree_arr = len(tree_arr)
 for i in range(size_tree_arr):
     size_line = len(tree_arr[i])
     for j in range(size_line):
-        if check_left(tree_arr, j, i) \
-                or check_right(tree_arr, j, i) \
-                or check_down(tree_arr, j, i) \
-                or check_up(tree_arr, j, i):
+        if check_left(tree_arr, i, j) \
+                or check_right(tree_arr, i, j) \
+                or check_down(tree_arr, i, j) \
+                or check_up(tree_arr, i, j):
             count_visible_trees += 1
 print(count_visible_trees)
